807214740c3fc29c212fb9f306e955b1a70cdb9e
cunlifs <s_cunliffe@uk.ibm.com>
2021-02-11 17:08:43 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -53,7 +53,12 @@ resource "openstack_blockstorage_volume_v3" "volume" {
 }
 
 resource "openstack_compute_volume_attach_v2" "va_1" {
-  volume_id  = openstack_blockstorage_volume_v3.volume[*].id
+  volume_id  = openstack_blockstorage_volume_v3.volume[0].id
+  instance_id  = openstack_compute_instance_v2.smc-vm[0].id
+}
+
+resource "openstack_compute_volume_attach_v2" "va_2" {
+  volume_id  = openstack_blockstorage_volume_v3.volume[1].id
   instance_id  = openstack_compute_instance_v2.smc-vm[0].id
 }
 
