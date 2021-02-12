d3adc71430f15c890587a8fab9533e52bb1d99e2
cunlifs <s_cunliffe@uk.ibm.com>
2021-02-12 09:47:16 +0000

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,7 +1,7 @@
 output "VM_name" {
-  value = openstack_compute_instance_v2.smc-vm.*.name
+  value = openstack_compute_instance_v2.app-vms.*.name
 }
 
 output "VM_IPs" {
-    value = openstack_compute_instance_v2.smc-vm.*.access_ip_v4
+    value = openstack_compute_instance_v2.app-vms.*.access_ip_v4
 }
