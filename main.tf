1013ff2f79ad9fe1b98f57514b0e2df49d37b4e1
cunlifs <s_cunliffe@uk.ibm.com>
2021-02-12 11:17:02 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -64,7 +64,7 @@ resource "openstack_compute_volume_attach_v2" "va_2" {
 
 # This runs after volume attach finishes
 resource "null_resource" "check_storage" {
-  depends_on = ["openstack_compute_volume_attach_v2.va_2"]
+  depends_on = [openstack_compute_volume_attach_v2.va_2]
   count = length(openstack_compute_instance_v2.app-vms.*.id)
   connection {
     host = element(openstack_compute_instance_v2.app-vms.*.access_ip_v4, count.index)
